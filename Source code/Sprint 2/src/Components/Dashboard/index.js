1a5b68012816453147a0c87eadc357ff5f314d4c
vishnuramv <vishnu.v1902@gmail.com>
2022-11-17 00:19:07 +0530

    added chart

--- Source code/Sprint 2/src/Components/Dashboard/index.js
+++ Source code/Sprint 2/src/Components/Dashboard/index.js
@@ -5,6 +5,10 @@ import { Box, Button, Card, CardActionArea, CardContent, CardHeader, CircularPro
 import Header from '../Header';
 import SideBar from '../SideBar';
 import InfoCard from '../InfoCard';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function CircularProgressWithLabel(
     props
@@ -60,6 +64,32 @@ function BudgetDialog(props) {
         </Dialog>
     );
 }
+export const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.5)',
+                'rgba(54, 162, 235, 0.5)',
+                'rgba(255, 206, 86, 0.5)',
+                'rgba(75, 192, 192, 0.5)',
+                'rgba(153, 102, 255, 0.5)',
+                'rgba(255, 159, 64, 0.5)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
 
 function Dashboard() {
     const [hasBudget, setHasBudget] = useState(false)
@@ -133,7 +163,8 @@ function Dashboard() {
                         </CardContent>
                     </Card>
                     <Card elevation={5} className='UserGraph'>
-                        <img src='https://karaandnate.com/wp-content/uploads/2018/01/Screen-Shot-2018-01-29-at-8.34.34-AM.png' alt='alter' className='Image' />
+                        <Doughnut className='dashgraph' data={data} />
+                        {/* <img src='https://karaandnate.com/wp-content/uploads/2018/01/Screen-Shot-2018-01-29-at-8.34.34-AM.png' alt='alter' className='Image' /> */}
                     </Card>
                 </div>
             </div>
