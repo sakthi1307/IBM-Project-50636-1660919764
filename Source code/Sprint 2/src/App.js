70c88c496a38584a3bd50b09970ccdf72dff159e
vishnuramv <vishnu.v1902@gmail.com>
2022-11-18 00:01:02 +0530

    updated cors

--- Source code/Sprint 2/src/App.js
+++ Source code/Sprint 2/src/App.js
@@ -7,18 +7,50 @@ import Signup from './Pages/Signup';
 import BillDashboard from './Pages/BillDashboard';
 import BudgetDashboard from './Pages/BudgetDashboard';
 import RecordDashboard from './Pages/RecordDashboard';
+import { userStore } from './store';
+import { useEffect } from 'react';
 
 function App() {
+  const isLoggedIn = userStore.useState(s => s.isLoggedIn);
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if (!!token) {
+      userStore.update(s => {
+        s.isLoggedIn = true
+        s.token = token
+      })
+      fetch('http://localhost:5000/getinfo', {
+        method: 'GET',
+        headers: new Headers({
+          "x-access-token": token
+        })
+      }).then(res => res.json().then(data => {
+        console.log(data)
+        userStore.update(s => {
+          s.user = data.users
+        })
+      }))
+    }
+  }, [])
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route index element={<Dashboard />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        <Route path="bills" element={<BillDashboard />} />
-        {/* <Route path="budgets" element={<BudgetDashboard />} /> */}
-        <Route path="records" element={<RecordDashboard />} />
+        {
+          !isLoggedIn ? (
+            <>
+              <Route path="/" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </>
+          ) : (
+            <>
+              <Route index element={<Dashboard />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route path="bills" element={<BillDashboard />} />
+              {/* <Route path="budgets" element={<BudgetDashboard />} /> */}
+              <Route path="records" element={<RecordDashboard />} />
+            </>
+          )
+        }
       </Routes>
     </div>
 
