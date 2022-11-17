e4d670a22a8f58f1d3792013493cd4e3540e0023
sakthi1307 <sakthi.enk@gmail.com>
2022-11-18 03:16:35 +0530

    db2

--- Source code/Sprint 4/backend/app.py
+++ Source code/Sprint 4/backend/app.py
@@ -2,15 +2,22 @@ from flask import Flask, request, jsonify, make_response,current_app
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from sqlalchemy import extract
-
+from flask_cors import CORS
+import ibm_db
+import ibm_db_sa
 from  werkzeug.security import generate_password_hash, check_password_hash
 # imports for PyJWT authentication
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
-  
+from waitress import serve
+from ibm_db_alembic.ibm_db import IbmDbImpl
+
+
 # creates Flask object
 app = Flask(__name__)
+CORS(app)
+
 # configuration
 # NEVER HARDCODE YOUR CONFIGURATION IN YOUR CODE
 
@@ -18,8 +25,11 @@ app = Flask(__name__)
 # INSTEAD CREATE A .env FILE AND STORE IN IT
 app.config['SECRET_KEY'] = 'your secret key'
 # database name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
+# db = SQLAlchemy.create_engine("db2+ibm_db://ctc88742:2gR5h8SYMcQEcj72@19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud[:30699]/bludb")
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'ibm_db_sa://ctc88742:2gR5h8SYMcQEcj72@19af6446-6171-4641-8aba-9dcff8e1b6ff.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud:30699/bludb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # creates SQLALCHEMY object
 db = SQLAlchemy(app)
 # Database ORMs
@@ -98,10 +108,10 @@ def token_required(f):
   
         try:
             # decoding the payload to fetch the stored details
-            print("received token : ",token)
-            print(app.config['SECRET_KEY'])
+            # print("received token : ",token)
+            # print(app.config['SECRET_KEY'])
             data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=["HS256"])
-            print("data",data)
+            # print("data",data)
             current_user = User.query\
                 .filter_by(public_id = data['public_id'])\
                 .first()
@@ -189,7 +199,7 @@ def login():
         # generates the JWT Token
         token = jwt.encode({
             'public_id': user.public_id,
-            'exp' : datetime.utcnow() + timedelta(minutes = 30)
+            'exp' : datetime.utcnow() + timedelta(minutes = 24*60*10)
         }, app.config['SECRET_KEY'],algorithm="HS256")
   
         res = make_response(jsonify({'token' : token}), 201)
@@ -237,6 +247,7 @@ def signup():
         db.session.commit()
   
         res = make_response('Successfully registered.', 201)
+        res.headers['Access-Control-Allow-Origin'] = '*'
     else:
         # returns 202 if user already exists
         res = make_response('User already exists. Please Log in.', 202)
@@ -244,13 +255,13 @@ def signup():
     return res
 
 
-@app.route('/getbills', methods =['GET'])
+@app.route('/bills', methods =['GET'])
 @token_required
 def get_bills(current_user):
     bills = Bills.query.filter_by(user=current_user.public_id).all()
     if bills is None:
         bills = []
-    res = make_response(jsonify({'records':[i.serialize for i in bills]}),201)
+    res = make_response(jsonify({'bills':[i.serialize for i in bills]}),201)
     res.headers['Access-Control-Allow-Origin'] = '*'
     return res
 
@@ -265,7 +276,7 @@ def get_record(current_user):
     res.headers['Access-Control-Allow-Origin'] = '*'
     return res
 
-@app.route('/putrecords', methods =['POST'])
+@app.route('/records', methods =['POST'])
 @token_required
 def put_record(current_user):
     form = request.form
@@ -293,7 +304,7 @@ def put_record(current_user):
     return res
 
 
-@app.route('/putbills', methods =['POST'])
+@app.route('/bills', methods =['POST'])
 @token_required
 def put_bills(current_user):
     form = request.form
@@ -333,16 +344,30 @@ def dashboard(current_user):
         last_month_spending =  Record.query.filter_by(user=current_user.public_id).filter(db.extract('month',Record.date_created)==12,db.extract('year',Record.date_created)==dt.year-1).all()
     income = current_user.income if current_user.income is not None else 0
     balance = income - current_month_spending
-    record_this_month.join(Record.category)
-    
-    
-    response_obj = {'last_week_spending':last_week_spending}
-    return response_obj
+    by_category = record_this_month.filter_by(gain=False).with_entities(Record.category, db.func.sum(Record.amount)).group_by(Record.category).all()
+    category_list = {}
 
+    for x,y in by_category:
+        category_list[x] = y
+    response_obj = make_response(jsonify({'last_week_spending':last_week_spending,'expense_by_category':category_list,'income':income,'balance':balance,'monthly_limit':current_user.monthly_limit,'current_month_spending':current_month_spending,'last_month_spending':last_month_spending}),201)
+    response_obj.headers['Access-Control-Allow-Origin'] = '*'
+    return  response_obj
 
+@app.route('/budget',methods=['POST'])
+@token_required
+def addbudget(current_user):
+    form = request.form
+    limit = form.get('budget')
+    current_user.monthly_limit = limit
+    db.session.add(current_user)
+    db.session.commit()
+    res = make_response("sucessfully added budget",201)
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    return res
 @app.before_first_request
 def create_tables():
     db.create_all()
+
 
 
 
@@ -350,5 +375,5 @@ if __name__ == "__main__":
     # setting debug to True enables hot reload
     # and also provides a debugger shell
     # if you hit an error while running the server
-    app.run(debug = True,host="0.0.0.0")
-
+    # app.run(host="0.0.0.0")
+    serve(app, listen='*:5000')
